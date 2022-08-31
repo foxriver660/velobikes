1c4a303843d2b0b487a855b995b5a068a6a7edef
foxriver661 <foxriver660@gmail.com>
2022-08-31 13:38:17 +0300

    add some variables

--- modules/scripts/small-dropdown-menu.js
+++ modules/scripts/small-dropdown-menu.js
@@ -1,53 +1,60 @@
 const dropDownBtn = document.querySelector(".velobikes__dropdown-btn");
 const dropDownList = document.querySelector(".velobikes__dropdown-list");
-const dropDownListItems = document.querySelectorAll(".velobikes__dropdown-item");
-const dropDownInput = document.querySelector(".velobikes__dropdown-hidden-input");
+const dropDownListItems = document.querySelectorAll(
+  ".velobikes__dropdown-item"
+);
+const dropDownInput = document.querySelector(
+  ".velobikes__dropdown-hidden-input"
+);
 
 function activeListItem(index) {
-  dropDownListItems.forEach((dropDownListItem) => dropDownListItem.classList.remove('velobikes__dropdown-item_active'));
-  dropDownListItems[index].classList.add('velobikes__dropdown-item_active')
+  dropDownListItems.forEach((dropDownListItem) =>
+    dropDownListItem.classList.remove("velobikes__dropdown-item_active")
+  );
+  dropDownListItems[index].classList.add("velobikes__dropdown-item_active");
 }
-function chooseSlide(){
+function chooseSlide() {
   let i = 0;
-  if(dropDownBtn.textContent == "Шоссе") {
+  if (dropDownBtn.textContent == "Шоссе") {
     sliderContainer.style.transform = `translateX(-${i * 130}%)`;
-  changeVelobikes(i);
-  activeListItem(i)
+    changeVelobikes(i);
+    activeListItem(i);
   }
-  if(dropDownBtn.textContent == "Грэвел") {
-    i++
-    sliderContainer.style.transform = `translateX(-${i * 130}%)`; 
-  changeVelobikes(i);
-  activeListItem(i)}
-  if(dropDownBtn.textContent == "TT") {
-    i+=2
-    sliderContainer.style.transform = `translateX(-${i * 130}%)`; 
-  changeVelobikes(i);
-  activeListItem(i)}
+  if (dropDownBtn.textContent == "Грэвел") {
+    i++;
+    sliderContainer.style.transform = `translateX(-${i * 130}%)`;
+    changeVelobikes(i);
+    activeListItem(i);
+  }
+  if (dropDownBtn.textContent == "TT") {
+    i += 2;
+    sliderContainer.style.transform = `translateX(-${i * 130}%)`;
+    changeVelobikes(i);
+    activeListItem(i);
+  }
 }
 
-dropDownBtn.addEventListener('click', function (e) {
-  dropDownList.classList.toggle('velobikes__dropdown-list_visible');
-  dropDownBtn.classList.add('velobikes__dropdown-btn_active');
+dropDownBtn.addEventListener("click", function (e) {
+  dropDownList.classList.toggle("velobikes__dropdown-list_visible");
+  dropDownBtn.classList.add("velobikes__dropdown-btn_active");
 });
 
 dropDownListItems.forEach(function (item) {
-  item.addEventListener('click', function (e) {
-    activeIdx++
+  item.addEventListener("click", function (e) {
+    activeIdx++;
     e.stopPropagation();
     dropDownBtn.innerText = item.innerText;
     dropDownBtn.focus();
     dropDownInput.value = item.dataset.value;
-    dropDownList.classList.remove('velobikes__dropdown-list_visible');
-    chooseSlide()
+    dropDownList.classList.remove("velobikes__dropdown-list_visible");
+    chooseSlide();
     changeVelobikes(activeIdx);
   });
 });
 
-document.addEventListener('click', function (e) {
+document.addEventListener("click", function (e) {
   if (e.target !== dropDownBtn) {
-    dropDownBtn.classList.remove('velobikes__dropdown-btn_active');
-    dropDownList.classList.remove('velobikes__dropdown-list_visible');
+    dropDownBtn.classList.remove("velobikes__dropdown-btn_active");
+    dropDownList.classList.remove("velobikes__dropdown-list_visible");
   }
 });
-
